71f22c102310eed95c3940e0291bca2776ab155d
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-04-21 22:33:51 +0300

    feat: add necessary ConflictException exception cases

--- src/main/java/mg/hei/agrifed/agrifedapi/exception/ConflictException.java
+++
@@ -1,7 +0,0 @@
-package mg.hei.agrifed.agrifedapi.exception;
-
-public class ConflictException extends RuntimeException {
-    public ConflictException(String message) {
-        super(message);
-    }
-}
