61ac3031f670ff1aca8fe2026bbdc7be59336be1
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-04-21 22:37:02 +0300

    feat: add necessary ConflictException exception cases

---
+++ src/main/java/mg/hei/agrifed/agrifedapi/exception/ConflictException.java
@@ -0,0 +1,39 @@
+package mg.hei.agrifed.agrifedapi.exception;
+
+/**
+ * Exception levée lorsqu'il y a un conflit avec l'état actuel des données
+ */
+public class ConflictException extends RuntimeException {
+    
+    public ConflictException(String message) {
+        super(message);
+    }
+    
+    public static ConflictException emailAlreadyExists(String email) {
+        return new ConflictException(
+            "A member with email address '" + email + "' already exists in the federation. " +
+            "Please use another email address."
+        );
+    }
+    
+    public static ConflictException phoneAlreadyExists(String phone) {
+        return new ConflictException(
+            "A member with the phone number '" + phone + "' already exists in the federation. " +
+            "Please use another phone number"
+        );
+    }
+    
+    public static ConflictException collectivityNameAlreadyExists(String name) {
+        return new ConflictException(
+            "A collectivity with the name '" + name + "' already exists in the federation. " +
+            "Please use another collectivity name"
+        );
+    }
+    
+    public static ConflictException collectivityNumberAlreadyExists(long numero) {
+        return new ConflictException(
+            "Collectivity n° " + numero + " is already attributed. " +
+            "Please use another number"
+        );
+    }
+}
