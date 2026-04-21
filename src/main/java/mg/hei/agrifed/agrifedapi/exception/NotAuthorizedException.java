4b7ff4678fde9d0bbddacad616b4c8cbd3f5cf09
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-04-21 16:50:05 +0300

    feat: add exceptions

---
+++ src/main/java/mg/hei/agrifed/agrifedapi/exception/NotAuthorizedException.java
@@ -0,0 +1,7 @@
+package mg.hei.agrifed.agrifedapi.exception;
+
+public class NotAuthorizedException extends RuntimeException {
+    public NotAuthorizedException(String message) {
+        super(message);
+    }
+}
