d3e1727ec0b56c659c23e2a686eadd68b17d4a99
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-05-07 00:25:31 +0300

    feat: add statistics controller

---
+++ src/main/java/mg/hei/agrifed/agrifedapi/controller/StatisticsRestController.java
@@ -0,0 +1,37 @@
+package mg.hei.agrifed.agrifedapi.controller;
+
+import mg.hei.agrifed.agrifedapi.dto.CollectivityLocalStatisticsDto;
+import mg.hei.agrifed.agrifedapi.dto.CollectivityOverallStatisticsDto;
+import mg.hei.agrifed.agrifedapi.service.StatisticsService;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/collectivities")
+public class StatisticsRestController {
+
+    private final StatisticsService statisticsService;
+
+    public StatisticsRestController(StatisticsService statisticsService) {
+        this.statisticsService = statisticsService;
+    }
+
+    @GetMapping("/{id}/statistics")
+    public ResponseEntity<List<CollectivityLocalStatisticsDto>> getLocalStatistics(
+            @PathVariable String id,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to) {
+        return ResponseEntity.ok(statisticsService.getLocalStatistics(id, from, to));
+    }
+
+    @GetMapping("/statistics")
+    public ResponseEntity<List<CollectivityOverallStatisticsDto>> getOverallStatistics(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to) {
+        return ResponseEntity.ok(statisticsService.getOverallStatistics(from, to));
+    }
+}
